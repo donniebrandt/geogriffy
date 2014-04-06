763dfcc54de5d50aa26e2874cba1c527f108b573
Donnie Brandt <donniebrandt@icloud.com>
2014-04-05 22:31:46 -0400

    Refine class, update readme

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ geogriffy
 =========
 
 A class to simplify dealing with coordinates and distance.
+
+$ringgold    = new Geogriffy(34.911342,-85.1421489);
+$chattanooga = new Geogriffy(35.0982955,-85.2386909);
+
+$ringgold->bounds(5);
+$ringgold->distance($chattanooga);
---
+++ geogriffy.php
@@ -0,0 +1,51 @@
+<?php
+
+class Geogriffy
+{
+    protected $latitude;
+    protected $longitude;
+
+    public function __construct($latitude, $longitude)
+    {
+        $this->latitude  = $latitude;
+        $this->longitude = $longitude;
+    }
+
+    public function bounds($radius)
+    {
+        $equatorLatMiles = 69.172;
+        
+        $bounds['maxLatitude']  = $this->latitude + ($radius / $equatorLatMiles);
+        $bounds['minLatitude']  = $this->latitude - ($bounds['maxLatitude'] - $this->latitude);
+        $bounds['maxLongitude'] = $this->longitude + $radius / (cos($bounds['minLatitude'] * M_PI / 180) * $equatorLatMiles);
+        $bounds['minLongitude'] = $this->longitude - ($bounds['maxLongitude'] - $this->longitude);
+
+        return $bounds;
+    }
+
+    public function distance(Geogriffy $endPoint)
+    {
+        $earthRadiusMiles = 3963;
+        $distance         = 0;
+
+        // Degrees to radians
+        $startPointLatRad  = $this->latitude * M_PI / 180;
+        $startPointLongRad = $this->longitude * M_PI / 180;
+        $endPointLatRad    = $endPoint->latitude * M_PI / 180;
+        $endPointLongRad   = $endPoint->longitude * M_PI / 180;
+        
+        // Disparate points?
+        if (ABS($startPointLatRad - $endPointLatRad) > 0.00001 || ABS($startPointLongRad - $endPointLongRad) > 0.00001) {
+            $distance = 
+                sin($startPointLatRad) * sin($endPointLatRad)
+                + cos($startPointLatRad) * cos($endPointLatRad)
+                * cos($endPointLongRad - $startPointLongRad);
+
+            $distance = 
+                $earthRadiusMiles
+                * (-1 * atan($distance / sqrt(1 - $distance * $distance)) + M_PI / 2);
+        }
+
+        return $distance;
+    }
+}
