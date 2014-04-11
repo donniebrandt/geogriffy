844b29e0b696bc2452238059353b3653383b522f
Donnie Brandt <donniebrandt@icloud.com>
2014-04-11 14:54:04 -0400

    Something, apparently

--- geogriffy.php
+++ geogriffy.php
@@ -14,7 +14,7 @@ class Geogriffy
     public function bounds($radius)
     {
         $equatorLatMiles = 69.172;
-        
+
         $bounds['maxLatitude']  = $this->latitude + ($radius / $equatorLatMiles);
         $bounds['minLatitude']  = $this->latitude - ($bounds['maxLatitude'] - $this->latitude);
         $bounds['maxLongitude'] = $this->longitude + $radius / (cos($bounds['minLatitude'] * M_PI / 180) * $equatorLatMiles);
@@ -33,15 +33,15 @@ class Geogriffy
         $startPointLongRad = $this->longitude * M_PI / 180;
         $endPointLatRad    = $endPoint->latitude * M_PI / 180;
         $endPointLongRad   = $endPoint->longitude * M_PI / 180;
-        
+
         // Disparate points?
         if (ABS($startPointLatRad - $endPointLatRad) > 0.00001 || ABS($startPointLongRad - $endPointLongRad) > 0.00001) {
-            $distance = 
+            $distance =
                 sin($startPointLatRad) * sin($endPointLatRad)
                 + cos($startPointLatRad) * cos($endPointLatRad)
                 * cos($endPointLongRad - $startPointLongRad);
 
-            $distance = 
+            $distance =
                 $earthRadiusMiles
                 * (-1 * atan($distance / sqrt(1 - $distance * $distance)) + M_PI / 2);
         }
